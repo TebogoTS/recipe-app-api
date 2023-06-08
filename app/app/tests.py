4a874cd871a205198e1f8c96759e11f3c214f964
tebogoTS-at-413271701111 <tebogoTS-at-413271701111>
2023-06-08 19:18:15 +0200

    Fixed Liniting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,11 @@
+
 """
 Sample tests
 """
 from django.test import SimpleTestCase
 
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
@@ -13,7 +15,6 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test substracting numbers."""
